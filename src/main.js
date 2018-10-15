8f940db4c219fae2ba8387c9c4db102e631f9d54
Jean-Christophe Sirot <jean-christophe.sirot@docker.com>
2018-10-15 14:15:04 +0200

    Add google analytics
    Signed-off-by: Jean-Christophe Sirot <jean-christophe.sirot@docker.com>

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import router from './router'
 import VueResource from 'vue-resource'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(Vuetify)
 Vue.use(VueResource)
+Vue.use(VueAnalytics, {
+  id: 'UA-127507557-1'
+})
 
 Vue.config.productionTip = false
 
